f833f64de53945342b776820089f94681cd51979
Igor Prochazka <igor.prochazka@gmail.com>
2016-01-20 22:47:28 +0100

    Introduce “shopify_header” template tag

---
+++ session.py
@@ -0,0 +1,3 @@
+def get_shopify_context(request):
+	request.session.get('shopify_context')
+
---
+++ templatetags/shopify_auth.py
@@ -0,0 +1,19 @@
+import datetime
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.inclusion_tag('shopify_auth/shopify_header.html', takes_context=True)
+def shopify_header(context):
+    shopify_context = context.get('shopify_context')
+    if shopify_context:
+    	shop = shopify_context.get('shop', None)
+    else:
+    	shop = None
+    result = {
+        'api_key': settings.SHOPIFY_API_KEY,
+        'shop': shop
+    }
+    return result
+
